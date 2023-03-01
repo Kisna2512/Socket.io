87ae25616048bad86c2906c70d5c43856daf2832
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2023-03-01 08:43:20 +0000

    We are starting the connection at line 6

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,13 @@
 const app = require("express")();
 const server = require("http").createServer(app);
+
+const io = require("socket.io")(server)
+
+io.on("connection", (socket) => {
+    console.log("what is socket is: ", socket);
+    console.log("Socket is active");
+    socket.on("chat", (payload) => {
+        console.log("What is Payload: ", payload);
+        io.emit("chat",payload)
+    })
+})
